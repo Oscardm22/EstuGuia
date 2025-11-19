e8c458aff72814e5ec6dc26b67819ceb4e1f2f23
Oscardm22 <oscarj.rierav@gmail.com>
2025-11-19 11:40:50 -0400

    eliminar funciones que no se usaban

--- app/src/main/java/com/oscardm22/estuguia/domain/utils/ValidationResult.kt
+++ app/src/main/java/com/oscardm22/estuguia/domain/utils/ValidationResult.kt
@@ -8,17 +8,4 @@ package com.oscardm22.estuguia.domain.utils
 data class ValidationResult(
     val isValid: Boolean,
     val errorMessage: String? = null
-) {
-    companion object {
-        /**
-         * Crea un ValidationResult válido
-         */
-        fun valid(): ValidationResult = ValidationResult(isValid = true)
-
-        /**
-         * Crea un ValidationResult inválido con mensaje de error
-         */
-        fun invalid(errorMessage: String): ValidationResult =
-            ValidationResult(isValid = false, errorMessage = errorMessage)
-    }
-}
+)
